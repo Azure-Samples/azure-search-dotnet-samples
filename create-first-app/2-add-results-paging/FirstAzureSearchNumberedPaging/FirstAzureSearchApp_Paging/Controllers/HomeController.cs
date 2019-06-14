750d91f39c3682e0a9ce848347c5f1354c497357
PeterTurcan <v-pettur@microsoft.com>
2019-06-14 14:19:28 -0700

    basic and paging complete

---
+++ create-first-app/2-add-results-paging/FirstAzureSearchNumberedPaging/FirstAzureSearchApp_Paging/Controllers/HomeController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstAzureSearchApp_Paging.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+
+namespace FirstAzureSearchApp_Paging.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchData model)
+        {
+            try
+            {
+                // Ensure the search string is valid.
+                if (model.searchText == null)
+                {
+                    model.searchText = "";
+                }
+
+                // Make the search call for the first page.
+                await RunQueryAsync(model, 0, 0);
+
+                // Ensure temporary data is stored for the next call.
+                TempData["page"] = 0;
+                TempData["leftMostPage"] = 0;
+                TempData["searchfor"] = model.searchText;
+            }
+
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+            return View(model);
+        }
+
+        public async Task<ActionResult> Page(SearchData model)
+        {
+            try
+            {
+                int page;
+
+                switch (model.paging)
+                {
+                    case "prev":
+                        page = (int)TempData["page"] - 1;
+                        break;
+
+                    case "next":
+                        page = (int)TempData["page"] + 1;
+                        break;
+
+                    default:
+                        page = int.Parse(model.paging);
+                        break;
+                }
+
+                // Recover the leftMostPage.
+                int leftMostPage = (int)TempData["leftMostPage"];
+
+                // Recover the search text and search for the data for the new page.
+                model.searchText = TempData["searchfor"].ToString();
+
+                await RunQueryAsync(model, page, leftMostPage);
+
+                // Ensure Temp data is stored for next call, as TempData only stored for one call.
+                TempData["page"] = (object)page;
+                TempData["searchfor"] = model.searchText;
+                TempData["leftMostPage"] = model.leftMostPage;
+            }
+
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "2" });
+            }
+            return View("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static SearchServiceClient _serviceClient;
+        private static ISearchIndexClient _indexClient;
+        private static IConfigurationBuilder _builder;
+        private static IConfigurationRoot _configuration;
+
+        private void InitSearch()
+        {
+            // Create a configuration using the appsettings file.
+            _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _configuration = _builder.Build();
+
+            // Pull the values from the appsettings.json file.
+            string searchServiceName = _configuration["SearchServiceName"];
+            string queryApiKey = _configuration["SearchServiceQueryApiKey"];
+
+            // Create a service and index client.
+            _serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(queryApiKey));
+            _indexClient = _serviceClient.Indexes.GetClient("hotels");
+        }
+
+        private async Task<ActionResult> RunQueryAsync(SearchData model, int page, int leftMostPage)
+        {
+            InitSearch();
+
+            var parameters = new SearchParameters
+            {
+                   // Enter Hotel property names into this list so only these values will be returned.
+                   // If Select is empty, all values will be returned, which can be inefficient.
+                   Select = new[] { "HotelName", "Description" },
+                   SearchMode = SearchMode.All,
+
+                   // Skip past results that have already been returned.
+                   Skip = page * GlobalVariables.ResultsPerPage,
+
+                   // Take only the next page worth of results.
+                   Top = GlobalVariables.ResultsPerPage,
+
+                   // Include the total number of results.
+                   IncludeTotalResultCount = true,
+               };
+
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.resultList = await _indexClient.Documents.SearchAsync<Hotel>(model.searchText, parameters);
+
+            // This variable communicates the total number of pages to the view.
+            model.pageCount = ( (int)model.resultList.Count + GlobalVariables.ResultsPerPage - 1) / GlobalVariables.ResultsPerPage;
+
+            // This variable communicates the page number being displayed to the view.
+            model.currentPage = page;
+
+            // Calculate the range of page numbers to display.
+            if (page == 0)
+            {
+                leftMostPage = 0;
+            }
+            else
+               if (page <= leftMostPage)
+            {
+                // Trigger a switch to a lower page range.
+                leftMostPage = Math.Max(page - GlobalVariables.PageRangeDelta, 0);
+            }
+            else
+            if (page >= leftMostPage + GlobalVariables.MaxPageRange - 1)
+            {
+                // Trigger a switch to a higher page range.
+                leftMostPage = Math.Min(leftMostPage + GlobalVariables.PageRangeDelta, model.pageCount - GlobalVariables.MaxPageRange);
+            }
+            model.leftMostPage = leftMostPage;
+
+            // Calculate the number of page numbers to display.
+            model.pageRange = Math.Min(model.pageCount - leftMostPage, GlobalVariables.MaxPageRange);
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ create-first-app/2-add-results-paging/FirstAzureSearchNumberedPaging/FirstAzureSearchApp_Paging/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstAzureSearchApp_Paging.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
