83b6eb4331e14bda907f7fef9c2b6ce2652da378
PeterTurcan <v-pettur@microsoft.com>
2019-06-14 16:09:13 -0700

    type ahead complete

---
+++ create-first-app/3-add-typeahead/FirstAzureSearchApp_Typeahead/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstAzureSearchApp_Typeahead.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
