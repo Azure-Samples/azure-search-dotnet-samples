b61fc2017727431a5f49c458f5e74d7e62f35cf1
PeterTurcan <v-pettur@microsoft.com>
2019-06-18 12:16:27 -0700

    short project names

---
+++ create-first-app/2a-add-paging/NumberedPaging/Models/SearchData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Azure.Search.Models;
+
+namespace NumberedPaging.Models
+{
+    public static class GlobalVariables
+    {
+        public static int ResultsPerPage
+        {
+            get
+            {
+                return 3;
+            }
+        }
+        public static int MaxPageRange
+        {
+            get
+            {
+                return 5;
+            }
+        }
+
+        public static int PageRangeDelta
+        {
+            get
+            {
+                return 2;
+            }
+        }
+    }
+    public class SearchData
+    {
+        // The text to search for.
+        public string searchText { get; set; }
+
+        // The current page being displayed.
+        public int currentPage { get; set; }
+
+        // The total number of pages of results.
+        public int pageCount { get; set; }
+
+        // The left-most page number to display.
+        public int leftMostPage { get; set; }
+
+        // The number of page numbers to display - which can be less than MaxPageRange towards the end of the results.
+        public int pageRange { get; set; }
+
+        // Used when page numbers, or next or prev buttons, have been selected.
+        public string paging { get; set; }
+
+        // The list of results.
+        public DocumentSearchResult<Hotel> resultList;
+    }
+}
