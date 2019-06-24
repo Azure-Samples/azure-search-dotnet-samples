716040ad81df94e970d9031918a689d6622c0f82
PeterTurcan <v-pettur@microsoft.com>
2019-06-24 14:02:29 -0700

    orders nits

--- create-first-app/5-order-results/OrderResults/Controllers/HomeController.cs
+++ create-first-app/5-order-results/OrderResults/Controllers/HomeController.cs
@@ -79,13 +79,13 @@ namespace OrderResults.Controllers
                 for (int n = 0; n < model.resultList.Results.Count; n++)
                 {
                     // Calculate room rates.
-                    var cheapest = 10000d;
+                    var cheapest = 0d;
                     var expensive = 0d;
 
                     for (var r = 0; r < model.resultList.Results[n].Document.Rooms.Length; r++)
                     {
                         var rate = model.resultList.Results[n].Document.Rooms[r].BaseRate;
-                        if (rate < cheapest)
+                        if (rate < cheapest || cheapest == 0)
                         {
                             cheapest = (double)rate;
                         }
