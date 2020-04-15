b8ffdcb4c13fdf52c4aac6d379140df8ca6cf46e
Derek Legenzoff <dereklegenzoff@gmail.com>
2020-04-15 15:25:21 -0700

    adding initial indexing optimization code

---
+++ optimize-data-indexing/OptimizeDataIndexing/DataGenerator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AzSearchPerformance
+{
+    class DataGenerator
+    {
+        private static int id;
+
+        public DataGenerator()
+        {
+            id = 0;
+        }
+
+        private Hotel GetNewHotel(string size = "small")
+        {
+
+            Hotel hotel;
+
+            // 100,000 small hotels is approximately 12mb
+            if (size == "small")
+            {
+                hotel = new Hotel()
+                {
+                    HotelId = id.ToString(),
+                    HotelName = "Secret Point Motel",
+                    Description = "The hotel is ideally located on the main commercial artery of the city in the heart of New York. A few minutes away is Time's Square and the historic centre of the city, as well as other places of interest that make New York one of America's most attractive and cosmopolitan cities. ",
+                    DescriptionFr = "L'hôtel est idéalement situé sur la principale artère commerciale de la ville en plein cœur de New York. A quelques minutes se trouve la place du temps et le centre historique de la ville, ainsi que d'autres lieux d'intérêt qui font de New York l'une des villes les plus attractives et cosmopolites de l'Amérique.",
+                    Category = "Boutique",
+                    Tags = new[] { "pool", "air conditioning", "concierge" },
+                    ParkingIncluded = false,
+                    LastRenovationDate = new DateTimeOffset(1970, 1, 18, 0, 0, 0, TimeSpan.Zero),
+                    Rating = 3.6,
+                    Address = new Address()
+                    {
+                        StreetAddress = "677 5th Ave",
+                        City = "New York",
+                        StateProvince = "NY",
+                        PostalCode = "10022",
+                        Country = "USA"
+                    }
+                };
+            }
+            // 100,000 small hotels is approximately 76mb
+            else
+            {
+                hotel = new Hotel()
+                {
+                    HotelId = id.ToString(),
+                    HotelName = "Secret Point Motel",
+                    Description = "The hotel is ideally located on the main commercial artery of the city in the heart of New York. A few minutes away is Time's Square and the historic centre of the city, as well as other places of interest that make New York one of America's most attractive and cosmopolitan cities. The hotel is ideally located on the main commercial artery of the city in the heart of New York. A few minutes away is Time's Square and the historic centre of the city, as well as other places of interest that make New York one of America's most attractive and cosmopolitan cities. The hotel is ideally located on the main commercial artery of the city in the heart of New York. A few minutes away is Time's Square and the historic centre of the city, as well as other places of interest that make New York one of America's most attractive and cosmopolitan cities. The hotel is ideally located on the main commercial artery of the city in the heart of New York. A few minutes away is Time's Square and the historic centre of the city, as well as other places of interest that make New York one of America's most attractive and cosmopolitan cities. ",
+                    DescriptionFr = "L'hôtel est idéalement situé sur la principale artère commerciale de la ville en plein cœur de New York. A quelques minutes se trouve la place du temps et le centre historique de la ville, ainsi que d'autres lieux d'intérêt qui font de New York l'une des villes les plus attractives et cosmopolites de l'Amérique.L'hôtel est idéalement situé sur la principale artère commerciale de la ville en plein cœur de New York. A quelques minutes se trouve la place du temps et le centre historique de la ville, ainsi que d'autres lieux d'intérêt qui font de New York l'une des villes les plus attractives et cosmopolites de l'Amérique.L'hôtel est idéalement situé sur la principale artère commerciale de la ville en plein cœur de New York. A quelques minutes se trouve la place du temps et le centre historique de la ville, ainsi que d'autres lieux d'intérêt qui font de New York l'une des villes les plus attractives et cosmopolites de l'Amérique.L'hôtel est idéalement situé sur la principale artère commerciale de la ville en plein cœur de New York. A quelques minutes se trouve la place du temps et le centre historique de la ville, ainsi que d'autres lieux d'intérêt qui font de New York l'une des villes les plus attractives et cosmopolites de l'Amérique.",
+                    Category = "Boutique",
+                    Tags = new[] { "pool", "air conditioning", "concierge" },
+                    ParkingIncluded = false,
+                    LastRenovationDate = new DateTimeOffset(1970, 1, 18, 0, 0, 0, TimeSpan.Zero),
+                    Rating = 3.6,
+                    Address = new Address()
+                    {
+                        StreetAddress = "677 5th Ave",
+                        City = "New York",
+                        StateProvince = "NY",
+                        PostalCode = "10022",
+                        Country = "USA"
+                    }
+                };
+            }
+
+            id++;
+            return hotel;
+        }
+
+
+        public List<Hotel> GetHotels(int count, string size = "small")
+        {
+            List<Hotel> hotelList = new List<Hotel>();
+            for (int i = 0; i < count; i++)
+            {
+                hotelList.Add(GetNewHotel(size));
+            }
+
+            return hotelList;
+        }
+    }
+}
---
+++ optimize-data-indexing/OptimizeDataIndexing/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Search;
+using Microsoft.Extensions.Configuration;
+using Index = Microsoft.Azure.Search.Models.Index;
+
+namespace AzSearchPerformance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            IConfigurationRoot configuration = builder.Build();
+
+            SearchServiceClient serviceClient = CreateSearchServiceClient(configuration);
+
+            string indexName = configuration["SearchIndexName"];
+            ISearchIndexClient indexClient = serviceClient.Indexes.GetClient(indexName);
+
+            Console.WriteLine("{0}", "Deleting index...\n");
+            DeleteIndexIfExists(indexName, serviceClient);
+
+            Console.WriteLine("{0}", "Creating index...\n");
+            CreateIndex(indexName, serviceClient);
+
+            //Console.WriteLine("{0}", "Finding optimal batch size...\n");
+            UploadDataUsingSDK.TestVariousBatchSizes(indexClient, 3);
+
+            //Console.WriteLine("{0}", "Finding optimal number of threads...\n");
+            //UploadDataUsingSDK.TestVariousThreadCounts(indexClient, 20000, 1000, 3).Wait();
+
+            DataGenerator dg = new DataGenerator();
+            List<Hotel> hotels = dg.GetHotels(100000, "large");
+
+            Console.WriteLine("{0}", "Uploading using exponential backoff...\n");
+            //UploadDataUsingSDK.UploadUsingExponentialBackoff(indexClient, hotels, 1000, 12).Wait();
+
+            Console.WriteLine("{0}", "Complete.  Press any key to end application...\n");
+            Console.ReadKey();
+        }
+
+
+        // Create the search service client
+        private static SearchServiceClient CreateSearchServiceClient(IConfigurationRoot configuration)
+        {
+            string searchServiceName = configuration["SearchServiceName"];
+            string adminApiKey = configuration["SearchServiceAdminApiKey"];
+
+            SearchServiceClient serviceClient = new SearchServiceClient(searchServiceName, new SearchCredentials(adminApiKey));
+            return serviceClient;
+        }
+
+        // Delete an existing index to reuse its name
+        private static void DeleteIndexIfExists(string indexName, SearchServiceClient serviceClient)
+        {
+            if (serviceClient.Indexes.Exists(indexName))
+            {
+                serviceClient.Indexes.Delete(indexName);
+            }
+        }
+
+        // Create an index whose fields correspond to the properties of the Hotel class.
+        // The Address property of Hotel will be modeled as a complex field.
+        // The properties of the Address class in turn correspond to sub-fields of the Address complex field.
+        // The fields of the index are defined by calling the FieldBuilder.BuildForType() method.
+        private static void CreateIndex(string indexName, SearchServiceClient serviceClient)
+        {
+            var definition = new Index()
+            {
+                Name = indexName,
+                Fields = FieldBuilder.BuildForType<Hotel>()
+            };
+
+            serviceClient.Indexes.Create(definition);
+        }
+
+    }
+}
