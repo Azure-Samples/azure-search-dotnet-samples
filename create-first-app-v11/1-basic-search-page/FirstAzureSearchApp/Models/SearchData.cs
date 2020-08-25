583a448fdd2e2caecb2bf5dbd16fdc0ad0c6948c
Tom Yao <v-xuyao@microsoft.com>
2020-08-25 17:16:40 +0800

    Add subfolder v11 and populate it with 1-basic-search-page changes

---
+++ create-first-app-v11/1-basic-search-page/FirstAzureSearchApp/Models/SearchData.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Search.Documents.Models;
+using System.Collections.Generic;
+
+namespace FirstAzureSearchApp.Models
+{
+    public class SearchData
+    {
+        // The text to search for.
+        public string searchText { get; set; }
+
+        // The list of results.
+        public List<SearchResult<Hotel>> resultList; 
+    }
+}
