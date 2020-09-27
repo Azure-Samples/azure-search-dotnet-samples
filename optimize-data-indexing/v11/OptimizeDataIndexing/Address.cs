aa3552d1301591ee63e22842505313d77089945d
Frank Li (Wicresoft North America Ltd) <v-cheli@microsoft.com>
2020-09-27 15:45:40 +0800

    Updating optimize-data-indexing to T2 package.

---
+++ optimize-data-indexing/v11/OptimizeDataIndexing/Address.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Search.Documents.Indexes;
+using System;
+
+namespace OptimizeDataIndexing
+{
+    [Serializable]
+    public partial class Address
+    {
+        [SearchableField]
+        public string StreetAddress { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string City { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string StateProvince { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string PostalCode { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string Country { get; set; }
+    }
+}
---
+++ optimize-data-indexing/v11/OptimizeDataIndexing/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+using System;
+using System.Text.Json.Serialization;
+
+namespace OptimizeDataIndexing
+{
+    [Serializable]
+    public partial class Hotel
+    {
+        [SimpleField(IsFilterable = true, IsKey = true)]
+        public string HotelId { get; set; }
+
+        [SearchableField(IsSortable = true)]
+        public string HotelName { get; set; }
+
+        [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.EnMicrosoft)]
+        public string Description { get; set; }
+
+        [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.FrLucene)]
+        [JsonPropertyName("Description_fr")]
+        public string DescriptionFr { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string Category { get; set; }
+
+        [SearchableField(IsFilterable = true, IsFacetable = true)]
+        public string[] Tags { get; set; }
+
+        [SimpleField(IsFilterable= true, IsSortable = true, IsFacetable = true)]
+        public bool? ParkingIncluded { get; set; }
+
+        [SimpleField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public DateTimeOffset? LastRenovationDate { get; set; }
+
+        [SimpleField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public double? Rating { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
