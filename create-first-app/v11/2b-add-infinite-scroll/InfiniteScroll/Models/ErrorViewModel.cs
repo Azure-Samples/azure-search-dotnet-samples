9544d35d0c6e5dce1f59a2c9cab426dbafa05f09
Frank Li (Wicresoft North America Ltd) <v-cheli@microsoft.com>
2020-09-27 13:58:01 +0800

    Add 2b-add-infinite-scroll

---
+++ create-first-app/v11/2b-add-infinite-scroll/InfiniteScroll/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InfiniteScroll.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
