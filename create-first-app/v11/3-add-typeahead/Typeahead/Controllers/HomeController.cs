856ae102b602019b2d144e285773df1ac3321bec
Frank Li (Wicresoft North America Ltd) <v-cheli@microsoft.com>
2020-09-27 14:17:38 +0800

    Updating 3-add-typeahead to T2 package.

---
+++ create-first-app/v11/3-add-typeahead/Typeahead/Controllers/HomeController.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Typeahead.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents;
+using Azure;
+using Azure.Search.Documents.Models;
+
+namespace Typeahead.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchData model)
+        {
+            try
+            {
+                // Ensure the search string is valid.
+                if (model.searchText == null)
+                {
+                    model.searchText = "";
+                }
+
+                // Make the search call for the first page.
+                await RunQueryAsync(model, 0, 0);
+
+                // Ensure temporary data is stored for the next call.
+                TempData["page"] = 0;
+                TempData["leftMostPage"] = 0;
+                TempData["searchfor"] = model.searchText;
+            }
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+
+            return View(model);
+        }
+
+        public async Task<ActionResult> PageAsync(SearchData model)
+        {
+            try
+            {
+                int page;
+
+                switch (model.paging)
+                {
+                    case "prev":
+                        page = (int)TempData["page"] - 1;
+                        break;
+
+                    case "next":
+                        page = (int)TempData["page"] + 1;
+                        break;
+
+                    default:
+                        page = int.Parse(model.paging);
+                        break;
+                }
+
+                // Recover the leftMostPage.
+                int leftMostPage = (int)TempData["leftMostPage"];
+
+                // Recover the search text and search for the data for the new page.
+                model.searchText = TempData["searchfor"].ToString();
+
+                await RunQueryAsync(model, page, leftMostPage).ConfigureAwait(false);
+
+                // Ensure Temp data is stored for next call, as TempData only stored for one call.
+                TempData["page"] = (object)page;
+                TempData["searchfor"] = model.searchText;
+                TempData["leftMostPage"] = model.leftMostPage;
+            }
+            catch
+            {
+                return View("Error", new ErrorViewModel { RequestId = "2" });
+            }
+
+            return View("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static SearchIndexClient _indexClient ;
+        private static SearchClient _searchClient;
+        private static IConfigurationBuilder _builder;
+        private static IConfigurationRoot _configuration;
+
+        private void InitSearch()
+        {
+            // Create a configuration using the appsettings file.
+            _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _configuration = _builder.Build();
+
+            // Pull the values from the appsettings.json file.
+            string searchServiceUri = _configuration["SearchServiceUri"];
+            string queryApiKey = _configuration["SearchServiceQueryApiKey"];
+
+            // Create a service and index client.
+            _indexClient = new SearchIndexClient(new Uri(searchServiceUri), new AzureKeyCredential(queryApiKey));
+            _searchClient = _indexClient.GetSearchClient("hotels");
+        }
+        public async Task<ActionResult> SuggestAsync(bool highlights, bool fuzzy, string term)
+        {
+            InitSearch();
+
+            // Setup the suggest parameters.
+            var options = new SuggestOptions()
+            {
+                UseFuzzyMatching = fuzzy,
+                Size = 8,
+            };
+
+            if (highlights)
+            {
+                options.HighlightPreTag = "<b>";
+                options.HighlightPostTag = "</b>";
+            }
+
+            // Only one suggester can be specified per index. The name of the suggester is set when the suggester is specified by other API calls.
+            // The suggester for the hotel database is called "sg" and simply searches the hotel name.
+            var suggestResult = await _searchClient.SuggestAsync<Hotel>(term, "sg", options).ConfigureAwait(false);
+
+            // Convert the suggest query results to a list that can be displayed in the client.
+            List<string> suggestions = suggestResult.Value.Results.Select(x => x.Text).ToList();
+
+            // Return the list of suggestions.
+            return new JsonResult(suggestions);
+        }
+        public async Task<ActionResult> AutoCompleteAsync(string term)
+        {
+            InitSearch();
+
+            // Setup the autocomplete parameters.
+            var ap = new AutocompleteOptions()
+            {
+                Mode = AutocompleteMode.OneTermWithContext,
+                Size = 6
+            };
+            var autocompleteResult = await _searchClient.AutocompleteAsync(term, "sg", ap).ConfigureAwait(false);
+
+            // Convert the autocompleteResult results to a list that can be displayed in the client.
+            List<string> autocomplete = autocompleteResult.Value.Results.Select(x => x.Text).ToList();
+
+            return new JsonResult(autocomplete);
+        }
+        public async Task<ActionResult> AutoCompleteAndSuggestAsync(string term)
+        {
+            InitSearch();
+
+            // Setup the type-ahead search parameters.
+            var ap = new AutocompleteOptions()
+            {
+                Mode = AutocompleteMode.OneTermWithContext,
+                Size = 1,
+            };
+            var autocompleteResult = await _searchClient.AutocompleteAsync(term, "sg", ap);
+
+            // Setup the suggest search parameters.
+            var sp = new SuggestOptions()
+            {                
+                Size = 8,
+            };
+
+            // Only one suggester can be specified per index. The name of the suggester is set when the suggester is specified by other API calls.
+            // The suggester for the hotel database is called "sg" and simply searches the hotel name.
+            var suggestResult = await _searchClient.SuggestAsync<Hotel>(term, "sg", sp).ConfigureAwait(false);
+
+            // Create an empty list.
+            var results = new List<string>();
+
+            if (autocompleteResult.Value.Results.Count > 0)
+            {
+                // Add the top result for type-ahead.
+                results.Add(autocompleteResult.Value.Results[0].Text);
+            }
+            else
+            {
+                // There were no type-ahead suggestions, so add an empty string.
+                results.Add("");
+            }
+
+            for (int n = 0; n < suggestResult.Value.Results.Count; n++)
+            {
+                // Now add the suggestions.
+                results.Add(suggestResult.Value.Results[n].Text);
+            }
+
+            // Return the list.
+            return new JsonResult(results);
+        }
+        private async Task<ActionResult> RunQueryAsync(SearchData model, int page, int leftMostPage)
+        {
+            InitSearch();
+
+            var options = new SearchOptions
+            {
+                SearchMode = SearchMode.All,
+
+                // Skip past results that have already been returned.
+                Skip = page * GlobalVariables.ResultsPerPage,
+
+                // Take only the next page worth of results.
+                Size = GlobalVariables.ResultsPerPage,
+
+                // Include the total number of results.
+                IncludeTotalCount = true,
+            };
+
+            // Enter Hotel property names into this list so only these values will be returned.
+            // If Select is empty, all values will be returned, which can be inefficient.
+            options.Select.Add("HotelName");
+            options.Select.Add("Description");
+
+            // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
+            model.resultList = await _searchClient.SearchAsync<Hotel>(model.searchText, options).ConfigureAwait(false);
+           
+            // This variable communicates the total number of pages to the view.
+            model.pageCount = ((int)model.resultList.TotalCount + GlobalVariables.ResultsPerPage - 1) / GlobalVariables.ResultsPerPage;
+
+            // This variable communicates the page number being displayed to the view.
+            model.currentPage = page;
+
+            // Calculate the range of page numbers to display.
+            if (page == 0)
+            {
+                leftMostPage = 0;
+            }
+            else if (page <= leftMostPage)
+            {
+                // Trigger a switch to a lower page range.
+                leftMostPage = Math.Max(page - GlobalVariables.PageRangeDelta, 0);
+            }
+            else if (page >= leftMostPage + GlobalVariables.MaxPageRange - 1)
+            {
+                // Trigger a switch to a higher page range.
+                leftMostPage = Math.Min(page - GlobalVariables.PageRangeDelta, model.pageCount - GlobalVariables.MaxPageRange);
+            }
+
+            model.leftMostPage = leftMostPage;
+
+            // Calculate the number of page numbers to display.
+            model.pageRange = Math.Min(model.pageCount - leftMostPage, GlobalVariables.MaxPageRange);
+
+            return View("IndexAsync", model);
+        }
+    }
+}
