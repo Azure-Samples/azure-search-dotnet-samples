e5f37cf053ed99f69fa454e6aec0c29c17991308
Frank Li (Wicresoft North America Ltd) <v-cheli@microsoft.com>
2020-09-27 14:50:30 +0800

    Updating index-backup-restore to T2 package.

---
+++ index-backup-restore/v11/AzureSearchBackupRestoreIndex/AzureSearchHelper.cs
@@ -0,0 +1,71 @@
+﻿//Copyright 2019 Microsoft
+
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+
+//       http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AzureSearchBackupRestore
+{
+    public class AzureSearchHelper
+    {
+        public const string ApiVersionString = "api-version=2019-05-06";
+
+        private static readonly JsonSerializerOptions _jsonOptions;
+
+        static AzureSearchHelper()
+        {
+            _jsonOptions = new JsonSerializerOptions { };
+
+            _jsonOptions.Converters.Add(new JsonStringEnumConverter());
+        }
+
+        public static string SerializeJson(object value)
+        {
+            return JsonSerializer.Serialize(value, _jsonOptions);
+        }
+
+        public static T DeserializeJson<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json, _jsonOptions);
+        }
+
+        public static HttpResponseMessage SendSearchRequest(HttpClient client, HttpMethod method, Uri uri, string json = null)
+        {
+            UriBuilder builder = new UriBuilder(uri);
+            string separator = string.IsNullOrWhiteSpace(builder.Query) ? string.Empty : "&";
+            builder.Query = builder.Query.TrimStart('?') + separator + ApiVersionString;
+
+            var request = new HttpRequestMessage(method, builder.Uri);
+
+            if (json != null)
+            {
+                request.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            }
+
+            return client.SendAsync(request).Result;
+        }
+
+        public static void EnsureSuccessfulSearchResponse(HttpResponseMessage response)
+        {
+            if (!response.IsSuccessStatusCode)
+            {
+                string error = response.Content == null ? null : response.Content.ReadAsStringAsync().Result;
+                throw new Exception("Search request failed: " + error);
+            }
+        }
+    }
+}
