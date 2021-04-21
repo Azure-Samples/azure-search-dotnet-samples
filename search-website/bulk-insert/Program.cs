0ed96fb78764207086fe96af9d7fc191b6cb32c6
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-04-21 14:59:07 -0700

    Fix

--- search-website/bulk-insert/Program.cs
+++ search-website/bulk-insert/Program.cs
@@ -14,8 +14,8 @@ namespace ConsoleApp1
     class Program
     {
         const string BOOKS_URL = "https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/books.csv";
-        const string SEARCH_ENDPOINT = "https://diberry-cognitive-search-website-tutorial.search.windows.net";
-        const string SEARCH_KEY = "25616BFEF5D0398D97E01D851069CDEA";
+        const string SEARCH_ENDPOINT = "https://YOUR-SEARCH-RESOURCE-NAME.search.windows.net";
+        const string SEARCH_KEY = "YOUR-SEARCH-ADMIN-KEY";
         const string SEARCH_INDEX_NAME = "good-books";
 
         static void Main(string[] args)
