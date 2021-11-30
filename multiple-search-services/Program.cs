87fdbeb84ba6481cd09b2496d73215d41e7e01eb
Matt <matthew.gotteiner@microsoft.com>
2021-11-30 13:48:00 -0800

    initial commit

---
+++ multiple-search-services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multiple_search_services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
