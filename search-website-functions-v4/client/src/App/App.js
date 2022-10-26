37ce3f9f7ae07b3f3b16403207e9e767f943c124
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-10-26 08:49:19 -0700

    Update search-website-functions-v4/client/src/App/App.js
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- search-website-functions-v4/client/src/App/App.js
+++ search-website-functions-v4/client/src/App/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Context for user authentication
