f5a23b34722bfb3ecaf8382351726ea084498984
HeidiSteen <heidist@microsoft.com>
2023-03-03 12:43:41 -0800

    Updated create-first-app lesson 1 to .NET 6

--- create-first-app/v11/1-basic-search-page/FirstAzureSearchApp/Controllers/HomeController.cs
+++ create-first-app/v11/1-basic-search-page/FirstAzureSearchApp/Controllers/HomeController.cs
@@ -6,12 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FirstAzureSearchApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -29,20 +37,14 @@ namespace FirstAzureSearchApp.Controllers
                 }
 
                 // Make the Azure Cognitive Search call.
-                await RunQueryAsync(model).ConfigureAwait(false);
+                await RunQueryAsync(model);
             }
+
             catch
             {
                 return View("Error", new ErrorViewModel { RequestId = "1" });
             }
-
             return View(model);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
         private static SearchClient _searchClient;
@@ -52,11 +54,11 @@ namespace FirstAzureSearchApp.Controllers
 
         private void InitSearch()
         {
-            // Create a configuration using the appsettings file.
+            // Create a configuration using appsettings.json
             _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
             _configuration = _builder.Build();
 
-            // Pull the values from the appsettings.json file.
+            // Read the values from appsettings.json
             string searchServiceUri = _configuration["SearchServiceUri"];
             string queryApiKey = _configuration["SearchServiceQueryApiKey"];
 
@@ -69,8 +71,8 @@ namespace FirstAzureSearchApp.Controllers
         {
             InitSearch();
 
-            var options = new SearchOptions() 
-            { 
+            var options = new SearchOptions()
+            {
                 IncludeTotalCount = true
             };
 
@@ -80,10 +82,21 @@ namespace FirstAzureSearchApp.Controllers
             options.Select.Add("Description");
 
             // For efficiency, the search call should be asynchronous, so use SearchAsync rather than Search.
-            model.resultList = await _searchClient.SearchAsync<Hotel>(model.searchText, options).ConfigureAwait(false);          
+            model.resultList = await _searchClient.SearchAsync<Hotel>(model.searchText, options).ConfigureAwait(false);
 
             // Display the results.
             return View("Index", model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- create-first-app/v11/1-basic-search-page/FirstAzureSearchApp/Program.cs
+++ create-first-app/v11/1-basic-search-page/FirstAzureSearchApp/Program.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace FirstAzureSearchApp
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
