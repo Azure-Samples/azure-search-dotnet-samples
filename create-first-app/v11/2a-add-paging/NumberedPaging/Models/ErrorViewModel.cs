3d71c531ab0b8a94c615ec70546144ff3cfe1377
HeidiSteen <heidist@microsoft.com>
2023-03-15 11:17:03 -0700

    Removed an archived sample, create-first-app

--- create-first-app/v11/2a-add-paging/NumberedPaging/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NumberedPaging.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ create-mvc-app/SearchPagingNew/SearchPagingNew/Models/SearchData.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Search.Documents.Models;
+
+namespace SearchPagingNew.Models
+{
+    public class SearchData
+    {
+        public string searchText { get; set; }
+
+        public SearchResults<Hotel> resultList;
+    }
+}
