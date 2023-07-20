b866f06ad507198872b2f2ba6d1b83c6bd00855d
HeidiSteen <heidist@microsoft.com>
2023-07-20 12:06:30 -0700

    Commit made from VS Code Azure Static Web Apps

---
+++ search-website-functions-v4/client-v4/src/components/AppFooter/AppFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './AppFooter.css';
+
+export default function AppFooter() {
+  return (
+      <footer className="footer">
+        <hr />
+        &copy; 2023 Microsoft
+      </footer>
+  );
+};
